0c3f1658c301abb6c5f6984bc1dc84961cb11f9f
GopalAror <punjabijanu586@gmail.com>
2024-02-29 09:46:21 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,25 +4,30 @@ let icon2 = document.querySelector(".icon2");
 let icon3 = document.querySelector(".icon3");
 
 function view() {
-    MobileView.classList.toggle("left-view");
-    document.querySelector("body").classList.toggle("overflow-hide")
-    icon1.classList.toggle("icon4");
-    icon2.classList.toggle("icon5");
-    icon3.classList.toggle("icon6");
+  MobileView.classList.toggle("left-view");
+  document.querySelector("body").classList.toggle("overflow-hide")
+  icon1.classList.toggle("icon4");
+  icon2.classList.toggle("icon5");
+  icon3.classList.toggle("icon6");
 }
 function scrollBtn() {
-    window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
+}
+window.addEventListener("scroll", function () {
+  let topbtn = document.getElementById("topbtn");
+  if (window.scrollY > 200) {
+    topbtn.style.display = 'block';
   }
-  window.addEventListener("scroll", function () {
-    let topbtn = document.getElementById("topbtn");
-    if (window.scrollY > 200) {
-        topbtn.style.display = 'block';
-    }
-    else{
-        topbtn.style.display = 'none';
-    }
-  })
-  setTimeout(() => {
-    document.getElementById("preloder").classList.add("d-none");
-    document.body.classList.remove("overflow-hidden");
-  }, 3000);
+  else {
+    topbtn.style.display = 'none';
+  }
+})
+setTimeout(() => {
+  document.getElementById("preloder").classList.add("d-none");
+  document.body.classList.remove("overflow-hidden");
+}, 3000);
+function size() {
+
+  document.title = (('NexAi') + "(" +`${innerWidth}` +")" + "(" + `${innerHeight}` + ")")
+}
+window.onresize = size
